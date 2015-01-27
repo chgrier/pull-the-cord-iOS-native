4d0bd3e830b36f172d389b9a6436942e87b4685c
Charles Grier <chgrier@mac.com>
2015-01-27 07:18:33 -0800

    Initial Commit

---
+++ Pull the Cord Native/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Pull the Cord Native
+//
+//  Created by Charles Grier on 1/27/15.
+//  Copyright (c) 2015 Grier Mobile Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
